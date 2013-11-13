413c95ea86468336b3d92d138c964edc25a33033
Michael Schubert <schubert.michael.j@gmail.com>
2013-11-12 18:02:13 -0600

    Changed keypoint to rich display and fixed .DS_Store bug

--- sift.py
+++ sift.py
@@ -5,25 +5,30 @@ import os
 import copy
 from astropy.io import fits
 
-def runSiftJPG(f):
+def runSiftJPG(f, count):
     img = cv2.imread(f.name)
     out = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #if output has 'order' BGR, otherwise RGB
     sift = cv2.SIFT()
 
+
     kp = sift.detect(out, None)
 
-    img = cv2.drawKeypoints(out, kp)
-    name = 'sifted' + str(f.name)[5:]
+    #img = cv2.drawKeypoints(out, kp)
+    img = cv2.drawKeypoints(out,kp,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    name = 'sifted' + str(count) + '.jpg'
+
+
+
     path  = os.path.dirname(os.path.abspath(__file__)) + '/processed_data/'
     fullpath = os.path.join(path, name)
-
     cv2.imwrite(fullpath, img)
 
+
+
+'''
 def runSiftFits(f,filename):
-
     mat = f[0].data
-
     #out = cv2.cvtColor(mat, cv2.COLOR_BGR2GRAY)
     mat = mat.astype(np.uint8)
 
@@ -40,19 +45,27 @@ def runSiftFits(f,filename):
     #cv2.imwrite(fullpath, img)
     #fcopy = copy.deepcopy(f)
     #fcopy[0].data = img
-    fits.writeto(filename, img)
-
-
+    fits.writeto('processed_data/' + filename, img)
+'''
 
 newpath = os.path.dirname(os.path.abspath(__file__)) + '/processed_data'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
+
+count = 0
+for filename in os.listdir('caslenses/'):
+    count += 1
+    if filename.endswith('.jpeg') or filename.endswith('.jpg'):
+        with open('caslenses/' + filename, 'rb') as f:
+            runSiftJPG(f, count)
+            print filename
 '''
-for filename in os.listdir('data/'):
-    with open('data/' + filename) as f:
-        runSift(f)
+for filename in os.listdir('FITS files/positive/g'):
+    with fits.open('FITS files/positive/g/' + filename) as f:
+        runSiftFits(f, filename)
 '''
 
-for filename in os.listdir('negative/g/'):
-    with fits.open('negative/g/' + filename) as f:
-        runSiftFits(f, filename)
+
+
+
+
